4659e5356680c98f261d985cf85c35e94a7e6de6
n9Mtq4 <bill.wresnahan@gmail.com>
2020-10-11 01:31:52 -0400

    Convert multiple thresholds to geotiff

--- png2geotiff.py
+++ png2geotiff.py
@@ -46,24 +46,30 @@ def write_gtiff(array, gdal_obj, outputpath, dtype=gdal.GDT_UInt16, options=0, c
     dest = None
 
 
+def proc_thresholdimg(nnt_path, outt_path, ref):
+    nnt = gdal.Open(nnt_path)
+    nnt_data = nnt.GetRasterBand(1).ReadAsArray()
+    write_gtiff(nnt_data, ref, outt_path, options=['COMPRESS=DEFLATE', 'PREDICTOR=2', 'ZLEVEL=9'], nodata=0)
+
+
 if __name__ == '__main__':
     
     # name = "LC08_L1TP_044005_20160713_20170222_01_T1"
     name = list(Path("./in/").glob("*.TIF"))[0].stem[:-3]  # remove band info from name
     
     nn_path = f"./out/{name}_NN.png"
-    nnt_path = f"./out/{name}_NNT.png"
     ref_path = f"./in/{name}_B1.TIF"
     
     out_path = f"./out/{name}_NN.TIF"
-    outt_path = f"./out/{name}_NNT.TIF"
     
     nn = gdal.Open(nn_path)
-    nnt = gdal.Open(nnt_path)
     ref = gdal.Open(ref_path)
     
     nn_data = nn.GetRasterBand(1).ReadAsArray()
-    nnt_data = nnt.GetRasterBand(1).ReadAsArray()
     
-    write_gtiff(nnt_data, ref, outt_path, options=['COMPRESS=DEFLATE', 'PREDICTOR=2', 'ZLEVEL=9'], nodata=255)
     write_gtiff(nn_data, ref, out_path, options=['COMPRESS=DEFLATE', 'PREDICTOR=2', 'ZLEVEL=9'])
+    
+    for t in [2, 5, 7]:
+        nnt_path = f"./out/{name}_NNT{t}.png"
+        outt_path = f"./out/{name}_NNT{t}.TIF"
+        proc_thresholdimg(nnt_path, outt_path, ref)
